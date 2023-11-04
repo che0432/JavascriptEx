d91997f11041891b5633d497d34a3dcd6f221456
Hye-eun Cho <cho0432@gmail.com>
2023-11-05 08:36:52 +0900

    css 추가

--- js/weather.js
+++ js/weather.js
@@ -10,7 +10,7 @@ function onGeoOk(position){
             const weather = document.querySelector("#weather span:first-child");
             const city = document.querySelector("#weather span:last-child");
             city.innerText = data.name;
-            weather.innerText = `${data.weather[0].description} / ${data.main.temp}`;
+            weather.innerText = `${data.weather[0].description} / ${data.main.temp}℃`;
         });
 }
 function onGeoError(){
